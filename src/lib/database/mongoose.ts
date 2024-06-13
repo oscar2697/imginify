d9bf5f911e5a8901b4b4337358c03c577c3190d9
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-06-13 13:49:16 -0500

    URL

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose} from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
 
 interface MongooseConnection {
     conn: Mongoose | null
@@ -16,9 +16,9 @@ if (!cached) {
 export const connectToDatabase = async () => {
     if (!cached.conn) return cached.conn
 
-    if (!MONGODB_URL) throw new Error('MONGODB_URL is not defined')
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined')
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName: 'imaginify', bufferCommands: false
     })
 
